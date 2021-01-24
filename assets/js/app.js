0374ea7229f1677fe3820902ab0bad25240bbea3
Luz Vazquez Galindo <luzvazgal@MacBook-Air-de-Luz.local>
2021-01-24 14:41:12 -0600

    Adding SVG object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,53 @@
 // @TODO: YOUR CODE HERE!
 
+// Settting up svg canvas size
+let svgWidth = 960
+let svgHeight = 500
+
+//Create SVG object
+d3.select('#scatter').append('svg').attr("height", svgHeight).attr("width", svgWidth)
+
+
+//Setting up where the scatter plot (an SVG chart group object) will be located
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+}
+
+let width = svgWidth - margin.left - margin.right
+let height = svgHeight - margin.top - margin.bottom
+
+
    //Creating arrays to store values from data
-   let x_poverty = []
+   //let x_poverty = []
    let x_age = []
-   let x_income = []
+   //let x_income = []
    
-   let y_obese = []
+   //let y_obese = []
    let y_smokes = []
-   let y_healthcare = []
+   //let y_healthcare = []
 
 //Retrieing data from CSV file
 d3.csv("assets/data/data.csv").then(function(states){
-   // console.log(states)
+  
   //Storing values from data in arrays
     states.forEach(state =>{
-        x_poverty.push(parseInt(state.poverty));
-        x_age.push(parseInt(state.age));
-        x_income.push(parseInt(state.income));
-        y_obese.push(parseInt(state.obese));
-        y_smokes.push(parseInt(state.smokes));
-        y_healthcare.push(parseInt(state.healthcare));
+        //x_poverty.push(parseInt(state.poverty));
+        let x_age = x_age.push(parseInt(state.age));
+        //x_income.push(parseInt(state.income));
+        //y_obese.push(parseInt(state.obese));
+        let y_smokes = y_smokes.push(parseInt(state.smokes));
+        //y_healthcare.push(parseInt(state.healthcare));
+
+        
+        
+
     });
+
+
+
 
     //Building Plot Income vs Smokes
     paint_plot();
@@ -44,44 +71,7 @@ let random_y
  * Draw scatter plot Income vs Smokes
  */
 function paint_plot(){
-
-    console.log(x_income)
-    console.log(y_smokes)
-    var trace1 = {
-        x: x_income,
-        y: y_smokes,
-        mode: 'markers',
-        type: 'scatter',
-        /*text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-        textposition: 'top center',
-        textfont: {
-          family:  'Raleway, sans-serif'
-        },
-        marker: { size: 12 }*/
-      };
-
-      var data = [ trace1 ];
-
-      /*var layout = {
-        xaxis: {
-          domain: [0, Math.max(x_income)],
-          range: [ 0, 25 ]
-        },
-        yaxis: {
-          domain: [0, Math.max(y_smokes)],
-          range: [0, 10]
-        },
-        /*legend: {
-          y: 0.5,
-          yref: 'paper',
-          font: {
-            family: 'Arial, sans-serif',
-            size: 20,
-            color: 'grey',
-          }
-        },
-        title:'Data Labels on the Plot'
-      };*/
-      
-      Plotly.newPlot('scatter', data);
+    d3.select('scatter')
+    
+   
 }
